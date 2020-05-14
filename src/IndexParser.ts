98bcf74f968b1a84f70e06b2f441671d7f80b69b
Alex <encalado.masia@gmail.com>
2020-05-14 18:24:57 +0200

    WIP

--- src/IndexParser.ts
+++ src/IndexParser.ts
@@ -5,3 +5,11 @@
 //(96699|TECHNICAL COMMUNICATIONS CORP|10-K|2019-12-13|edgar/data/96699/0001171843-19-008104.txt)
 
 //GUARDEM EN UN FITXER TEMPORAL TOTS ELS SELECTS
+
+class IndexParser {
+  constructor() {}
+  getUrl(year: string, quarter: string) {
+    let url = `https://www.sec.gov/Archives/edgar/full-index/'${year}'/'${quarter}'/xbrl.idx`;
+    console.log(`[IndexParser]: ${url} `);
+  }
+}
