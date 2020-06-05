dac08334cb6f997dd840fe1127cc9c18ddb90690
nikensss <rikitzzz@gmail.com>
2020-06-06 01:02:47 +0200

    modified tests.ts just to try out how to make a status report

--- src/tests/tests.ts
+++ src/tests/tests.ts
@@ -1,29 +1,44 @@
-import fs, { WriteStream } from 'fs';
-import { Writable } from 'stream';
-import chalk from 'chalk';
-class TestClass {
-  public readonly data: string;
-  constructor(d: string) {
-    this.data = d;
+// import fs, { WriteStream } from 'fs';
+// import { Writable } from 'stream';
+// import chalk from 'chalk';
+// class TestClass {
+//   public readonly data: string;
+//   constructor(d: string) {
+//     this.data = d;
+//   }
+
+//   printClassName() {
+//     console.log(this.constructor.name);
+//   }
+// }
+
+// const t: TestClass = new TestClass('data that was given');
+
+// t.printClassName();
+// const ws: Writable = fs.createWriteStream('./logs/test.log', { flags: 'w' });
+// const stdout: Writable = process.stdout;
+// ws.write(new Date().toLocaleString() + '\n');
+// ws.write('writting stuff to created write stream\n');
+// ws.write('second write\n');
+// ws.write('third write\n');
+// ws.write('fourth write\n');
+// ws.write('fifth write\n');
+// stdout.write(chalk.red('writting stuff to stdout\n'));
+// stdout.write(chalk.red('writting stuff to stdout\n'));
+// stdout.write(chalk.red('writting stuff to stdout\n'));
+// stdout.write(chalk.red('writting stuff to stdout\n'));
+
+let i: number = 0;
+const interval = setInterval(() => {
+  process.stdout.clearLine(0);
+  process.stdout.cursorTo(0);
+  process.stdout.write('Status: ' + i + '%');
+  i += Math.round(Math.random() * 10);
+  if (i > 100) {
+    clearInterval(interval);
+    process.stdout.clearLine(0);
+    process.stdout.cursorTo(0);
+    process.stdout.write('Status: 100%');
+    process.stdout.write('\n');
   }
-
-  printClassName() {
-    console.log(this.constructor.name);
-  }
-}
-
-const t: TestClass = new TestClass('data that was given');
-
-t.printClassName();
-const ws: Writable = fs.createWriteStream('./logs/test.log', { flags: 'w' });
-const stdout: Writable = process.stdout;
-ws.write(new Date().toLocaleString() + '\n');
-ws.write('writting stuff to created write stream\n');
-ws.write('second write\n');
-ws.write('third write\n');
-ws.write('fourth write\n');
-ws.write('fifth write\n');
-stdout.write(chalk.red('writting stuff to stdout\n'));
-stdout.write(chalk.red('writting stuff to stdout\n'));
-stdout.write(chalk.red('writting stuff to stdout\n'));
-stdout.write(chalk.red('writting stuff to stdout\n'));
+}, 100);
