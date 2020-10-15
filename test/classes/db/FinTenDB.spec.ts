d695212146fe2516dbbd4894af758e754c685b91
faraday <encalado.masia@gmail.com>
2020-10-15 16:32:03 +0000

    Deleting comments

--- test/classes/db/FinTenDB.spec.ts
+++ test/classes/db/FinTenDB.spec.ts
@@ -1,33 +1,5 @@
-// import chai, { expect } from 'chai';
-// import chaiAsPromised from 'chai-as-promised';
-// import mongoose from 'mongoose';
-// import { MongoMemoryServer } from 'mongodb-memory-server';
+
 
 describe('FinTenDB tests', () => {
-  // before(done => {
-  //   mongo
-  //     .getUri()
-  //     .then(uri => {
-  //       console.log(`URI is ${uri}`);
-  //       return mongoose.connect(uri, options);
-  //     })
-  //     .then(() => {
-  //       done();
-  //     });
-  // });
-  // it('should count 0 filings', done => {
-  //   const db = FinTenDB.getInstance()
-  //     .then(db => {
-  //       db.use(mongoose);
-  //       return FilingSchema.count({});
-  //     })
-  //     .then(count => {
-  //       count; //?
-  //       expect(count).to.equal(0);
-  //       done();
-  //     });
-  // });
-  // after(() => {
-  //   console.log('Closing connections!');
-  // });
+
 });
