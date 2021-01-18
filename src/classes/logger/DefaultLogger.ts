62f1f9bfb3f35f268fd6fe3f45a851703578602c
Ricard_MBP16 <rikitzzz@gmail.com>
2021-01-18 22:24:41 +0100

    fix: improved loglevel class

--- src/classes/logger/DefaultLogger.ts
+++ src/classes/logger/DefaultLogger.ts
@@ -3,7 +3,6 @@ import { Logger } from './Logger.interface';
 import { LogLevel } from './LogLevel';
 import { Writable } from 'stream';
 import moment from 'moment';
-import chalk from 'chalk';
 
 class DefaultLogger implements Logger {
   private _logLevel: LogLevel = LogLevel.INFO;
@@ -43,49 +42,38 @@ class DefaultLogger implements Logger {
   }
 
   debug(...messages: unknown[]): void {
-    if (this._logLevel <= LogLevel.DEBUG) {
-      this.log('DEBUG', messages);
+    if (LogLevel.DEBUG.isLowerPriorityThan(this._logLevel)) {
+      return;
     }
+    this.log(LogLevel.DEBUG, messages);
   }
 
   info(...messages: unknown[]): void {
-    if (this._logLevel <= LogLevel.INFO) {
-      this.log('INFO', messages);
+    if (LogLevel.INFO.isLowerPriorityThan(this._logLevel)) {
+      return;
     }
+    this.log(LogLevel.INFO, messages);
   }
 
   warning(...messages: unknown[]): void {
-    if (this._logLevel <= LogLevel.WARNING) {
-      this.log('WARNING', messages);
+    if (LogLevel.WARNING.isLowerPriorityThan(this._logLevel)) {
+      return;
     }
+    this.log(LogLevel.WARNING, messages);
   }
 
   error(...messages: unknown[]): void {
-    if (this._logLevel <= LogLevel.ERROR) {
-      this.log('ERROR', messages);
+    if (LogLevel.ERROR.isLowerPriorityThan(this._logLevel)) {
+      return;
     }
+    this.log(LogLevel.ERROR, messages);
   }
 
-  private log(type: 'DEBUG' | 'INFO' | 'WARNING' | 'ERROR', ...args: unknown[]): void {
+  private log(level: LogLevel, ...args: unknown[]): void {
     const now = moment().format(DefaultLogger.MOMENT_FORMAT);
-    const color = this.getColor(type);
+    const color = level.getColor();
     const message = args.map((a) => JSON.stringify(a, this.circularReferenceHelper(), 2)).join(';');
-    this.output.write(color(`${now}|{${type}} [${this.label}] ${message}\n`));
-  }
-
-  private getColor(level: 'DEBUG' | 'INFO' | 'WARNING' | 'ERROR'): chalk.Chalk {
-    switch (level) {
-      case 'DEBUG':
-        return chalk.gray;
-      case 'INFO':
-        return chalk.blueBright;
-      case 'WARNING':
-        return chalk.yellow;
-      case 'ERROR':
-        return chalk.red;
-      default:
-        return chalk.gray;
-    }
+    this.output.write(color(`${now}|{${level.getLevel()}} [${this.label}] ${message}\n`));
   }
 
   private circularReferenceHelper() {
--- src/classes/logger/LogLevel.ts
+++ src/classes/logger/LogLevel.ts
@@ -1,6 +1,50 @@
-export const enum LogLevel {
-  DEBUG,
-  INFO,
-  WARNING,
-  ERROR
+import chalk from 'chalk';
+
+export type Level = 'SILLY' | 'DEBUG' | 'INFO' | 'WARNING' | 'ERROR';
+export class LogLevel {
+  private level: Level;
+  private priority: number;
+  private color: chalk.Chalk;
+
+  private constructor(level: Level, priority: number, color: chalk.Chalk) {
+    this.level = level;
+    this.priority = priority;
+    this.color = color;
+  }
+
+  getLevel(): Level {
+    return this.level;
+  }
+
+  getPriority(): number {
+    return this.priority;
+  }
+
+  getColor(): chalk.Chalk {
+    return this.color;
+  }
+
+  isLowerPriorityThan(logLevel: LogLevel): boolean {
+    return this.getPriority() < logLevel.getPriority();
+  }
+
+  static get SILLY(): LogLevel {
+    return new LogLevel('SILLY', 0, chalk.gray);
+  }
+
+  static get DEBUG(): LogLevel {
+    return new LogLevel('DEBUG', 1, chalk.gray);
+  }
+
+  static get INFO(): LogLevel {
+    return new LogLevel('INFO', 2, chalk.blueBright);
+  }
+
+  static get WARNING(): LogLevel {
+    return new LogLevel('WARNING', 3, chalk.yellow);
+  }
+
+  static get ERROR(): LogLevel {
+    return new LogLevel('ERROR', 4, chalk.red);
+  }
 }
